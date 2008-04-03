0d207de533c8e48d8ae62e7f38baf01801f213a4
gunark <gunark@323d99bf-2bc9-4860-b5ae-bc2019f3fcb7>
2008-04-03 19:45:53 +0000

    git-svn-id: svn+ssh://rubyforge.org/var/svn/restr/trunk@2 323d99bf-2bc9-4860-b5ae-bc2019f3fcb7

---
+++ README.txt
@@ -0,0 +1,44 @@
+= Restr
+
+<b>Reststop is a very simple client for RESTful[http://en.wikipedia.org/wiki/Representational_State_Transfer] 
+web services.</b> It is developed as a lightweight alternative to 
+ActiveResource[http://api.rubyonrails.com/files/vendor/rails/activeresource/README.html]. 
+
+For project info and downloads please see http://rubyforge.org/projects/restr
+
+
+*Author*::    Matt Zukowski (matt at roughest dot net)
+*Copyright*:: Copyright (c) 2008 Urbacon Ltd.
+*License*::   GNU Lesser General Public License Version 3
+
+
+
+Restr is basically a wrapper around Ruby's Net::HTTP, offering
+a more RESTfully meaningful interface.
+
+See the Restr class documentation for more info, but here's a simple
+example of RESTful interaction with Restr:
+
+  require 'restr'
+  kitten = Restr.get('http://example.com/kittens/1.xml')
+  puts kitten['name']
+  puts kitten['colour']
+  
+  kitten['colour'] = 'black'
+  kitten = Restr.put('http://example.com/kittens/1.xml', kitten)
+
+
+== License
+
+Restr is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published 
+by the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+Reststop is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
---
+++ lib/restr.rb
@@ -0,0 +1,129 @@
+$:.unshift File.dirname(__FILE__)
+
+require 'net/http'
+require 'net/https'
+require 'uri'
+require 'cgi'
+
+begin
+  require 'xml_simple'
+rescue LoadError
+  require 'rubygems'
+  require 'active_support'
+  begin
+  require 'xml_simple'
+  rescue LoadError
+    require 'xmlsimple'
+  end
+end
+
+
+# A very simple REST client, best explained by example:
+#
+#   # Retrieve a Kitten and print its name and colour
+#   kitten = Restr.get('http://example.com/kittens/1.xml')
+#   puts kitten['name']
+#   puts kitten['colour']
+#
+#   # Create a Kitten
+#   kitten = Restr.post('http://example.com/kittens.xml', 
+#     :name => 'batman', :colour => 'black')
+#
+#   # Update a Kitten
+#   kitten = Restr.put('http://example.com/kittens/1.xml', 
+#     :age => '6 months')
+#
+#   # Delete a Kitten :(
+#   kitten = Restr.delete('http://example.com/kittens/1.xml')
+#
+#   # Retrieve a list of Kittens
+#   kittens = Restr.get('http://example.com/kittens.xml')
+#
+# When the response to a Restr request has content type 'text/xml', the
+# response body will be parsed from XML into a nested Hash (using XmlSimple 
+# -- see http://xml-simple.rubyforge.org/). Otherwise the response is  
+# returned untouched, as a String.
+#
+# If the remote REST resource requires authentication (Restr only supports
+# HTTP Basic authentication, for now):
+#
+#   Restr.get('http://example.com/kittens/1.xml, {}, 
+#     {:username => 'foo', :password => 'bar'})
+#
+class Restr
+  @@log = nil
+  
+  def self.logger=(logger)
+    @@log = logger.dup
+    # ActiveSupport's BufferedLogger doesn't seem to support progname= :(
+    @@log.progname = self.name if @@log.respond_to?(:progname)
+  end
+  
+  def self.method_missing(method, *args)
+    self.do(method, args[0], args[1] || {}, args[2])
+  end
+  
+  def self.do(method, url, params = {}, auth = nil)
+    uri = URI.parse(url)
+    params = {} unless params
+      
+    method_mod = method.to_s.downcase.capitalize
+    unless Net::HTTP.const_defined?(method_mod)
+      raise InvalidRequestMethod, 
+        "Callback method #{method.inspect} is not a valid HTTP request method."
+    end
+    
+    if method_mod == 'Get'
+      q = params.collect{|k,v| "#{CGI.escape(k.to_s)}=#{CGI.escape(v.to_s)}"}.join("&")
+      if uri.query
+        uri.query += "&#{q}"
+      else
+        uri.query = q
+      end
+    end
+    
+    req = Net::HTTP.const_get(method_mod).new(uri.request_uri)
+    
+    
+    if auth
+      raise ArgumentError, 
+        "The `auth` parameter must be a Hash with a :username and :password value." unless 
+        auth.kind_of? Hash
+      req.basic_auth auth[:username] || auth['username'], auth[:password] || auth['password']
+    end
+    
+    unless method_mod == 'Get'
+      req.set_form_data(params, '&')
+    end
+    
+    @@log.debug("Sending #{method.inspect} request to #{url.inspect} with data #{params.inspect}"+
+        (auth ? " with authentication" : "")+".") if @@log
+ 
+    client = Net::HTTP.new(uri.host, uri.port)
+    client.use_ssl = (uri.scheme == 'https')
+    res = client.start do |http|
+      http.request(req)
+    end
+    
+    case res
+    when Net::HTTPSuccess
+      if res.content_type == 'text/xml'
+        @@log.debug("Got XML response.") if @@log
+        return XmlSimple.xml_in_string(res.body,
+          'forcearray'   => false,
+          'keeproot'     => false
+        )
+      else
+        @@log.debug("Got #{res.content_type.inspect} response.") if @@log
+        return res.body
+      end
+    else
+      $LAST_ERROR_BODY = res.body # FIXME: this is dumb... need a better way of reporting errors
+      @@log.error("Got error response '#{res.message}(#{res.code})': #{$LAST_ERROR_BODY}") if @@log
+      res.error!      
+    end
+  end
+  
+  class InvalidRequestMethod < Exception
+  end
+end
